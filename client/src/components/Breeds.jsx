57e5dc2cb8fbd3cc6996db7f6b46af53dda33ec8
Braian Serra <bnserra@gmail.com>
2022-02-11 19:23:29 -0300

    initial commit

---
+++ client/src/components/Breeds.jsx
@@ -0,0 +1,10 @@
+import { React, Component } from "react";
+
+class Breeds extends Component{
+    render(){
+        return(
+            <h1>Este es el componente Breeds</h1>
+        )
+    }
+}
+        
