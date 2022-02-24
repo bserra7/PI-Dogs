237d58c04e611e32c5b7b4448af2e3cee80ac4d6
Braian Serra <bnserra@gmail.com>
2022-02-24 12:42:30 -0300

    Pagination active function correction

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -4,16 +4,18 @@ import s from '../css/Pagination.module.css';
 const Pagination = ({ breedsPerPage, currentPage, totalBreeds, paginate }) => {
     const pageNumbers = [];
     const [prevTarget, setPrevTarget] = useState('');
+    const [lastPage, setLastPage] = useState();
 
     for (let i = 1; i <= Math.ceil(totalBreeds / breedsPerPage); i++) {
         pageNumbers.push(i);
     }
 
     if(prevTarget && currentPage === 1) prevTarget.className = `${s.pages}`;
-
+    if(lastPage && currentPage === 1)  document.getElementById(lastPage)?.setAttribute('class', `${s.pages}`);
+    
     const handleActive = (event, number) => {
         if (prevTarget) prevTarget.className = `${s.pages}`;
-        document.getElementById('1')?.setAttribute('class', `${s.pages}`);
+        document.getElementById(currentPage)?.setAttribute('class', `${s.pages}`);
         event.target.className = `${s.pages} ${s.active}`;
         setPrevTarget(event.target);
         if (number < 1) return;
@@ -22,13 +24,26 @@ const Pagination = ({ breedsPerPage, currentPage, totalBreeds, paginate }) => {
 
     const handlePrevNextPage = (action) => {
         if ((currentPage <= 1 && action === 'prev') || (currentPage >= pageNumbers.length && action === 'next')) return;
-        else if (action === 'prev') paginate(currentPage - 1);
-        else if (action === 'next') paginate(currentPage + 1);
+        else if (action === 'prev'){
+            document.getElementById(currentPage)?.setAttribute('class', `${s.pages}`);
+            document.getElementById(currentPage - 1)?.setAttribute('class', `${s.pages} ${s.active}`);
+            setLastPage(currentPage - 1);
+            paginate(currentPage - 1);
+        } 
+        else if (action === 'next'){
+            document.getElementById(currentPage)?.setAttribute('class', `${s.pages}`);
+            document.getElementById(currentPage + 1)?.setAttribute('class', `${s.pages} ${s.active}`);
+            setLastPage(currentPage + 1);
+            paginate(currentPage + 1);
+        } 
     }
 
+    // Establezco el mismo largo de 5 numeros en el arreglo para no modificar el tamaño de la barra de paginación
     let shortPages = [];
     if (currentPage === 1) shortPages = pageNumbers.slice(currentPage - 1, currentPage + 4);
     else if (currentPage === 2) shortPages = pageNumbers.slice(currentPage - 2, currentPage + 3);
+    else if (currentPage === pageNumbers.length - 1) shortPages = pageNumbers.slice(currentPage - 4, currentPage + 1);
+    else if (currentPage === pageNumbers.length) shortPages = pageNumbers.slice(currentPage - 5, currentPage);
     else shortPages = pageNumbers.slice(currentPage - 3, currentPage + 2);
 
     return (
