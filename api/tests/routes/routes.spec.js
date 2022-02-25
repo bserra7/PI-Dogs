bc22cf23f16ed6b2b4cc2082efd30e42f3082505
Braian Serra <bnserra@gmail.com>
2022-02-25 18:00:43 -0300

    Test minor changes importation file

--- api/tests/routes/routes.spec.js
+++ api/tests/routes/routes.spec.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Dog, conn } = require('../../src/db.js');
